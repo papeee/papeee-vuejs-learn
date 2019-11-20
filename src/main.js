5e652707d5f93e266a8466f4d98c65dd8d17ef75
Pasi Rautiainen <n5613@student.jamk.fi>
2019-11-20 18:40:08 +0200

    updated lodash

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import sayHello from './modules/MyModule';
 import _ from 'lodash';
 
 const arr = _.concat([1, 2, 3], 4, [5]);
-sayHello('Hello from Rollup');
+sayHello('Hello from Rollup and Lodash' + arr);
