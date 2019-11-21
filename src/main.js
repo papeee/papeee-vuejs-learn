acd421617919dd2300f8044b45699f4955612dcf
Pasi Rautiainen <n5613@student.jamk.fi>
2019-11-21 12:49:46 +0200

    added using vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import sayHello from './modules/MyModule';
+import Vue from 'vue';
+import Input from './modules/MyModule';
+import message from './separate.vue';
 import _ from 'lodash';
 
-const arr = _.concat([1, 2, 3], 4, [5]);
-sayHello('Hello from Rollup and Lodash' + arr);
+//const arr = _.concat([1, 2, 3], 4, [5]);
+Input("<p>Type some animal or animals</p>",App);
+
+Vue.component('app-message',message);
