bfda04cf01016320fbdb15a1748a6573210c32bc
Pasi Rautiainen <n5613@student.jamk.fi>
2019-11-21 12:51:16 +0200

    vue component

---
+++ src/separate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <app-message><h2>{{ message }}</h2></app-message>
+    
+    <app-input>
+    <input type="text" v-model=" msg ">
+    </app-input>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: "Welcome to Wildlife in Vue App"
+      
+    }
+  },
+  components:{
+    'app-input': Input,
+    msg : ""
+  }
+}
+</script>
