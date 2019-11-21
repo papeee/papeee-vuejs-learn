657fb15d56bf7fb885403e11b1466ae99657f083
Pasi Rautiainen <n5613@student.jamk.fi>
2019-11-21 18:33:21 +0200

    useless away

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue';
+//import Vue from 'vue';
+import vue from 'rollup-plugin-vue'
 import Input from './modules/MyModule';
 import message from './separate.vue';
 import _ from 'lodash';
 
 //const arr = _.concat([1, 2, 3], 4, [5]);
-Input("<p>Type some animal or animals</p>",App);
+Input("<p>Type some animal or animals</p>");
 
 Vue.component('app-message',message);
